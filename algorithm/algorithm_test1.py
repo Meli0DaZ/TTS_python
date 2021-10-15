5970f655f8537b2f69807cc7225624c053184581
Meli0DaZ <trieuhaipham32621998@gmail.com>
2021-10-15 11:11:10 +0700

    Update algorithm_test1.py

--- algorithm/algorithm_test1.py
+++ algorithm/algorithm_test1.py
@@ -1,112 +1,176 @@
-# Segregate str to list
-def toList(a):
-    list = [str(i) for i in a]
-    return list
+# Turn input string into list.
+# Input: '123' => result: ['1', '2', '3']
+def toList(li):
+    return [str(i) for i in li]
 
-# If given list is [1, 2, 3]
-# then the subset would be [1, 2, 3, [1, 2], [2, 3], [1, 2, 3]]
+# For a given list of [1, 2, 3]
+# the new list with subsets would be    [['1'], ['2'], ['3'],
+#                                        ['1', '2'], ['2', '3'],
+#                                        ['1', '2', '3']]
 def continuousSubset(li):
-    if len(li)!=1:
-        root = continuousSubset(li[:-1])
-        op = li[-1:]
-        new = root
-        new.append(op)
-        # print(new)
-        for i in new:
-            if int(i[-1]) == int(op[0])-1:
-                new.append(i+op)
-                # print(new)
-        print(new)
-        return new
-    else:
-        return [[li[0]]]
+    result = []
+    for a in li:
+        result.append([a])
+        if len(result)>1:
+            for b in result:
+                if int(b[-1]) == int(a)-1:
+                    result.append(b+[a])
+    return result
 
-def finalSubset(li):
-    # Remove subsets have only 1 element like ['1'], ['2']
-    new_li = []
+# Remove subsets have only 1 element like ['1'], ['2']
+def removeSingle(li):
+    result = []
     for i in range(len(li)):
         if (len(li[i]) > 1):
-            new_li.append(li[i])
+            result.append(li[i])
+    return result
 
-    # Input [1,2] and [2,3] => Has a common element of 2 => Return TRUE
-    # Input [1,2] and [3,4] => Has a common element of 2 => Return FALSE
-    def checkCommon(li1,li2):
-        hasCommon = False
-        for i in li1:
-            for j in li2:
-                if set(i).intersection(set(j)):
-                    hasCommon = True
-                    break
-        return hasCommon
-    if len(new_li)!=0:
-        root = finalSubset(new_li[:-1])
-        op = new_li[-1:]
-        new = root.copy()
-        for i in new:
+# Check within 2 set if they have commen element, if they do => return True
+def checkCommon(li1, li2):
+    common = False
+    for i in li1:
+        for j in li2:
+            if set(i).intersection(set(j)):
+                common = True
+                break
+        if common:
+            break
+    return common
+
+# Group subsets that don't have common element
+# Subset1 ['1', '2'] and Subset2 ['3', '4'] => Result [[], ['1', '2'], ['3', '4'], [['1', '2'], ['3', '4']]]
+# Subset1 ['1', '2'] and Subset3 ['2', '3'] => Result [[], ['1', '2'], ['2', '3']]
+def stackSubset(li):
+    li = removeSingle(li)
+    if len(li)!=0:
+        op = li[-1:]
+        base = stackSubset(li[:-1])
+        result = base.copy()
+        for i in base:
             if not checkCommon(i,op):
-                new.append(i+op)
-        print(new)
-        return new
+                # print(i+op)
+                result.append(i+op)
+                # print(result)
     else:
         return [[]]
+    return result
 
-def convertToNumbers(stringNumberList):
-    # Example:
-    # input numbers = ['1','2'], end = 6 output: [['1','2'], '3', '4', 5]
-    # input numbers = ['5','6'], start = 1, end = 10  output: ['1','2', '3', '4', ['5','6'],'7','8','9']
-    def fillNumber(numbers, **kwargs):
-        startList = []
-        endList = []
-        if 'start' in kwargs.keys():
-            startList = [str(x) for x in range(kwargs['start'], int(numbers[0]))]
-        if 'end' in kwargs.keys():
-            endList = [str(x) for x in range(int(numbers[-1]) + 1, kwargs['end'])]
-        new_numbers = ["".join(numbers)]
-        result = startList + new_numbers + endList
-        return result
+# Fill the subset with the missing number
+# Subset: ['3', '4'] => Result: ['1', '2', '34', '5', '6', '7', '8', '9']
+def fillNumStr(strLi,**kwargs):
+    startLi = []
+    endLi = []
+    if 'start' in kwargs:
+        startLi = [str(i) for i in range(kwargs['start'], int(strLi[0]))]
+    if 'end' in kwargs:
+        endLi = [str(j) for j in range(int(strLi[-1]) + 1, kwargs['end'])]
+    num = [''.join(strLi)]
+    return startLi+num+endLi
 
-    if len(stringNumberList) == 1:
-        result = fillNumber(stringNumberList[0], start=1, end=10)
-        # print(stringNumberList)
-        # print(result)
-    elif len(stringNumberList) == 2:
-        result = fillNumber(stringNumberList[0], start=1) +\
-                 fillNumber(stringNumberList[1], start=int(stringNumberList[0][-1]) + 1, end=10)
-        # print(stringNumberList)
-        # print(result)
-    else:
-        new_list = []
-        for i in range(len(stringNumberList)):
-            if (i == 0):
-                new_list.append(fillNumber(stringNumberList[i], start=1))
-            elif (i == len(stringNumberList) - 1):
-                new_list.append(fillNumber(stringNumberList[i], end=10))
+# Elements in list are now string type data.
+# They need to all be converted to int type data for the next step.
+# Every subsets will be handled individually.
+# First, fill the each subset with the missing number.
+# Then, convert each element of every subset's new list to int type data.
+def strToInt(strLi):
+    result = []
+    if len(strLi) == 1:
+        result = fillNumStr(strLi[0], start=1, end=10)
+    elif len(strLi) == 2:
+        result += fillNumStr(strLi[0], start=1)
+        result += fillNumStr(strLi[1], start=int(strLi[0][-1]) + 1, end=10)
+    elif len(strLi) >=3:
+        for i in range(len(strLi)):
+            if i == 0:
+                result+= fillNumStr(strLi[i], start=1)
+            elif i == len(strLi)-1:
+                result+= fillNumStr(strLi[i], end=10)
             else:
-                new_list.append(fillNumber(stringNumberList[i],
-                                           start=int(stringNumberList[i - 1][-1]) + 1,
-                                           end=int(stringNumberList[i + 1][0])))
-        result = new_list
-        # print(stringNumberList)
-        # print(result)
+                result+= fillNumStr(strLi[i], start=int(strLi[i - 1][-1]) + 1, end=int(strLi[i + 1][0]))
 
-    try:
-        result = sum(result, [])
-        return [int(x) for x in result]
-    except:
-        return [int(x) for x in result]
+    return [int(x) for x in result]
 
-a='12345'
-b = toList(a)
+# Create a string of numbers, '+', '-', and '=' to print out.
+def resultStr(numLi, minusPosList, target):
+    result = str(numLi[0])
+    for i in range(1, len(numLi)):
+        if i in minusPosList:
+            result+= ' - ' + str(numLi[i])
+        else:
+            result+= ' + ' + str(numLi[i])
+        if i == len(numLi)-1:
+            result+= ' = ' + str(target)
+    return result
 
-subSetContinue = continuousSubset(b)
-# print(f'{subSetContinue}\n')
+# Find all subset within the input that have the sum = target and return a list of sets of the numbers' position
+# Input: [1,2,3,4,5,6,7,8,9] and target = 13 => result =    [[0, 1, 2, 3], [0, 1, 6], [0, 2, 5],
+#                                                            [0, 3, 4], [1, 2, 4], [2, 6], [3, 5]]
+def findMinusPos(numLi, target, result = [], minusPos = [], recursiveIndex = 1):
+    if not minusPos:
+        minusSet = []
+    else:
+        minusSet = [numLi[x] for x in minusPos]
+    s = sum(minusSet)
 
-# Find all possible sequen with None
-sequenceWithNone = finalSubset(subSetContinue)
-# print(f'{sequenceWithNone}\n')
-sequenceWithNone = sequenceWithNone[1:]
-# print(f'{sequenceWithNone}\n\n')
+    # We need to check whether the numbers' sum is = or > than the target
+    # If the sum is = target then add the lists of position the the result
+    # If the sum is < target, continue to the next recursive loop
+    # If the sum is >= target, end current recursive loop and go back to the previous one.
+    if s == target:
+        result.append(minusPos)
+    if s >= target:
+        return  # Return nothing to end the current recursive loop
 
-for se in sequenceWithNone:
-    se = convertToNumbers(se)
-    print(f'{se}\n')
+    # Everytime the loop starts in the recursive loop, the start of the 'for' loop increase by 1 and add a new minus position to the minus index list.
+    # If the sum has yet to reach target, minus position list will continue to add the next number's position (from the number list).
+    # If the sum is >= target, the last number's position in the minus position list will be replaced by the next one in the number list,
+    # and the process repeats until the end of the loop, then go back to the 'for' loop of the previous recursive loop.
+    for i in range(recursiveIndex, len(numLi)):
+        newMinusIndex = i
+        recursiveIndex += 1
+        findMinusPos(numLi, target, result, minusPos + [newMinusIndex], recursiveIndex)
+    return result
+
+# When adding '-' or '+' or nothing, first we need to get all the groups of number from the given string.
+# Then, determines whether to add '+' or '-'.
+# By defaul, we will add '+' to before all numbers (except for the 1st one).
+# After that, we check to see if the value of the result (the sum of the numbers) = target.
+# If the result is < target => discard it;
+# If the result is = target => print it out;
+# If the result is > target => check to see if we can change '+' to '-' to reduce the value,
+#                               If we can, print it out;
+#                               If we can't, discard it.
+def solution(numbers, target):
+    var = 0
+    # Find all continuous subsets and add them to the lists individually.
+    subsetsList = continuousSubset(toList(numbers))
+
+    # Removes all single elements (['1'], ['2'], ...), only keep elements like ['1', '2'] or ['1', '2', '3'],
+    # and group the subsets that don't have common elements together.
+    # And remove the 1st element since it's empty [] (it's was created for the purpose of the program)
+    stackedSubsetsList = stackSubset(subsetsList)[1:]
+    stackedSubsetsList.append(toList(numbers))
+
+    for set in stackedSubsetsList:
+        set = strToInt(set)
+        
+        if sum(set) == target:
+            var += 1
+            print(f'{var}:\t{resultStr(set, [], target)}')
+        elif sum(set) < target:
+            continue
+
+        # When changing from '+' to '-', we need to decrease the value by 2*numbers.
+        # Example: 1+2+3+4+5=15 and 1+2+3+4-5=5.
+        # The difference before and after changing '+5' into '-5' is 10=2*5
+        else:
+            difference = abs(sum(set)-target)
+            minusPosList = findMinusPos([i*2 for i in set], difference, [])
+            if minusPosList:
+                for minusPos in minusPosList:
+                    var += 1
+                    print(f'{var}:\t{resultStr(set, minusPos, target)}')
+
+if __name__ == '__main__':
+    test = '123456789'
+    solution(test, 100)
